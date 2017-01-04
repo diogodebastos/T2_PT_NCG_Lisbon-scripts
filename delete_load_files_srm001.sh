c7cfefb608969718e8b01cb1686172660674b680
Pietro Vischia <pietro.vischia@gmail.com>
2017-01-04 12:15:28 +0000

    Updated load files with new paths

--- delete_load_files_srm001.sh
+++ delete_load_files_srm001.sh
@@ -7,7 +7,7 @@ size=0;
 #srmpath=srm://srm01.ncg.ingrid.pt:8444;
 srmpath=srm://srm01.ncg.ingrid.pt
 
-PHEDEX_X509_USER_PROXY=/var/lib/vobox/cms/proxy_repository/+2fC+3dPT+2fO+3dLIPCA+2fO+3dLIP+2fOU+3dLisboa+2fCN+3dPietro+20Vischia+2fCN+3dproxy-voms+20cms;
+PHEDEX_x509_USER_PROXY=/var/lib/vobox/cms/proxy_repository/+2fDC+3dch+2fDC+3dcern+2fOU+3dOrganic+20Units+2fOU+3dUsers+2fCN+3dcbeiraod+2fCN+3d719371+2fCN+3dCristovao+20Beirao+20Da+20Cruz+20E+20Silva+2fCN+3dproxy-voms+20cms
 
 PHEDEX_GLITE_ENV=/etc/profile.d/grid-env.sh;
 source $PHEDEX_GLITE_ENV;
@@ -19,7 +19,7 @@ MYPROXY_SERVER=myproxy-fts.cern.ch;
 export MYPROXY_SERVER;
 
 
-folder="/lustre/ncg.ingrid.pt/data2/cms/store/PhEDEx_LoadTest07"
+folder="/gstore/t2cms/cms/store/PhEDEx_LoadTest07"
 
 for file in `find $folder -name 'LoadTest07*' -type f -mmin +$min_max -print`; do
 
@@ -27,7 +27,7 @@ for file in `find $folder -name 'LoadTest07*' -type f -mmin +$min_max -print`; d
 
     echo "FILE is $file";
 
-    newfile=`echo $file | sed "s:/lustre/ncg.ingrid.pt/data2/:/:"`
+    newfile=`echo $file | sed "s:/gstore/t2cms/cms/:/:"`
 
     size=`stat -c %s $file 2>/dev/null`
 
