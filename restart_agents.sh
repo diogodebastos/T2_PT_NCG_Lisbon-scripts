bb0e10976f7cfbbfefcf263f1860ebc1271bbb91
Diogo de Bastos <dbastos@lip.pt>
2019-01-16 18:18:43 +0000

    change proxy directory

--- restart_agents.sh
+++ restart_agents.sh
@@ -3,7 +3,7 @@
 # by hand:
 # vobox-proxy --vo cms -proxy-safe 3600 --myproxy-safe 259200 --email pietro.vischia@gmail.com register
 
-PHEDEX_x509_USER_PROXY=/var/lib/vobox/cms/proxy_repository/+2fDC+3dch+2fDC+3dcern+2fOU+3dOrganic+20Units+2fOU+3dUsers+2fCN+3dcbeiraod+2fCN+3d719371+2fCN+3dCristovao+20Beirao+20Da+20Cruz+20E+20Silva+2fCN+3dproxy-voms+20cms;
+PHEDEX_x509_USER_PROXY=var/lib/vobox/cms/proxy_repository/+2fDC+3dch+2fDC+3dcern+2fOU+3dOrganic+20Units+2fOU+3dUsers+2fCN+3ddchasque+2fCN+3d826698+2fCN+3dDiogo+20BASTOS+2fCN+3dproxy-voms+20cms
 
 PHEDEX_GLITE_ENV=/etc/profile.d/grid-env.sh;
 
